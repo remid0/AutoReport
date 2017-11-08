b60ba9c455c3e14193248738bf4b107a2ebcf3cf
= <=>
2017-11-08 15:06:42 +0100

    make MABXCanReceiver and VehicleCanReceiver inherit from Process + delete odometer_time

--- RaspiClient/can_manager.py
+++ RaspiClient/can_manager.py
@@ -1,5 +1,4 @@
-from ctypes import c_uint, c_float
-from datetime import datetime
+from ctypes import c_uint
 from multiprocessing import Process
 from multiprocessing.sharedctypes import Value
 
@@ -8,81 +7,70 @@ from can.interface import Bus
 import settings
 
 
-class MABXCanReceiver(object):
+class MABXCanReceiver(Process):
 
-    @classmethod
-    def receive(cls, mode, mabx_bus):
+    def __init__(self, mode):
+        super(MABXCanReceiver, self).__init__()
+        self.mode = mode
+        self.mabx_bus = Bus(channel=settings.CAN_MABX_CHANNEL, bustype=settings.CAN_BUS_TYPE)
+
+    def run(self):
         while True:
-            message = mabx_bus.recv()
-
+            message = self.mabx_bus.recv()
             if message.arbitration_id == 0xC1:
-                mode.value = cls.decode_mode_value(message.data)
+                self.mode.value = self.decode_mode_value(message.data)
 
-    @classmethod
-    def decode_mode_value(cls, data):
+    def decode_mode_value(self, data):
         mask = 0xF000000
         return (int.from_bytes(data, byteorder='big') & mask) >> 24
 
 
-class VehicleCanReceiver(object):
+class VehicleCanReceiver(Process):
 
-    @classmethod
-    def receive(cls, odometer_value, odometer_time, vin, vehicle_state, vehicle_bus):
+    def __init__(self, odometer_value, vin, vehicle_state):
+        super(VehicleCanReceiver, self).__init__()
+        self.odometer_value = odometer_value
+        self.vin = vin
+        self.vehicle_state = vehicle_state
+        self.vehicle_bus = Bus(channel=settings.CAN_VEHICLE_CHANNEL, bustype=settings.CAN_BUS_TYPE)
+
+    def run(self):
         while True:
-            message = vehicle_bus.recv()
+            message = self.vehicle_bus.recv()
 
             if message.arbitration_id == 0x5D7:
-                odometer_value.value = cls.decode_odometer_value(message.data)
-                odometer_time.value = message.timestamp
+                self.odometer_value.value = self.decode_odometer_value(message.data)
 
             elif message.arbitration_id == 0x69F:
-                vin.value = cls.decode_vin_value(message.data)
+                self.vin.value = self.decode_vin_value(message.data)
 
             elif message.arbitration_id == 0x35C:
-                vehicle_state.value = cls.decode_vehicle_state_value(message.data)
+                self.vehicle_state.value = self.decode_vehicle_state_value(message.data)
 
-    @classmethod
-    def decode_odometer_value(cls, data):
+    def decode_odometer_value(self, data):
         mask = 0xFFFFF000
         return (int.from_bytes(data, byteorder='big') & mask) >> 12
 
-    @classmethod
-    def decode_vin_value(cls, data):
+    def decode_vin_value(self, data):
         pass
 
-    @classmethod
-    def decode_vehicle_state_value(cls, data):
+    def decode_vehicle_state_value(self, data):
         mask = 0x700000000000000
         return (int.from_bytes(data, byteorder='big') & mask) >> 56
-
-
-class OdometerInfo(object):
-    def __init__(self, value, timestamp):
-        self.value = value
-        self.datetime = datetime.utcfromtimestamp(timestamp)
-
 
 class CanManager(object):
     def __init__(self):
         #TODO : adapt to the SessionManager code
         self.odometer_value = Value(c_uint)
-        self.odometer_time = Value(c_float)
         self.vin = Value(c_uint)
         self.vehicle_state = Value(c_uint)
         self.mode = Value(c_uint)
 
-        mabx_bus = Bus(channel=settings.CAN_MABX_CHANNEL, bustype=settings.CAN_BUS_TYPE)
-        vehicle_bus = Bus(channel=settings.CAN_VEHICLE_CHANNEL, bustype=settings.CAN_BUS_TYPE)
-
-        mabx_process = Process(target=MABXCanReceiver.receive, args=(self.mode, mabx_bus))
+        mabx_process = MABXCanReceiver(self.mode)
         mabx_process.start()
-        vehicle_process = Process(target=VehicleCanReceiver.receive, args=(
-            self.odometer_value, self.odometer_time, self.vin, self.vehicle_state, vehicle_bus
-        ))
-        vehicle_process.start()
 
-    def get_odometer_info(self):
-        return OdometerInfo(self.odometer_value.value, self.odometer_time.value)
+        vehicle_process = VehicleCanReceiver(self.odometer_value, self.vin, self.vehicle_state)
+        vehicle_process.start()
 
     def get_vehicle_state(self):
         pass
