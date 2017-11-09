7f64ca34f19ff3b9b76b6036bb50338a674c4632
remid0 <remid.2002@gmail.com>
2017-11-10 00:50:23 +0100

    Bug correction

--- RaspiClient/tests/test_session_upload.py
+++ RaspiClient/tests/test_session_upload.py
@@ -4,19 +4,30 @@ import requests
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
+# Examples of GPS coordinates
+    # 49.42043, 2.79502 : N31
+    # 49.41271, 2.81436 : Boulevard
+    # 49.4183, 2.6944 : Autoroute du Nord
+    # 49.04874, 2.09101 : Rue des Marechaux
+    # 49.04851, 2.09222 : Rue des Marechaux
 
 payload = [{
+    'car': {'vin_code': 12},
     'start_date': datetime.utcnow().strftime(DATETIME_FORMAT),
     'stop_date': datetime.now().strftime(DATETIME_FORMAT),
     'mode': 'AUT',
     'distance': 20,
-    'users': [1, 2],  # it's an id from django store as server_pk in local database
-    'gps_traces': [
+    # it's an id from django store as server_pk in local database
+    'users': [3, 4],
+    'car': 1,
+    'gps_points': [
         {
             'datetime': datetime.utcnow().strftime(DATETIME_FORMAT),
-            'latitude': 10,
-            'longitude': 10,
-            'altitude': 10,
+            'latitude': 49.42043,
+            'longitude': 2.79502,
+            'altitude': 0,
+            'speed': 15,
+            'track': 26,
         },
     ]
 }]
