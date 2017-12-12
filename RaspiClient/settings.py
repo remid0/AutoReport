83f7acf8586de39e536f52df54193bb9e93bd545
remid0 <remid.2002@gmail.com>
2017-12-12 11:55:43 +0100

    output directory

--- RaspiClient/settings.py
+++ RaspiClient/settings.py
@@ -1,26 +1,32 @@
 from enum import Enum
 
+# Files
+OUTPUT_DIRECTORY = 'output'
 
+LAST_GPS_POINT_FILE = '%s/gps.sav' % OUTPUT_DIRECTORY
+LOG_FILE = '%s/debug.log' % OUTPUT_DIRECTORY
+SESSION_SAVE_FILE = '%s/sessions.sav' % OUTPUT_DIRECTORY
+SESSION_UPLOAD_FILE = '%s/to_upload_%d.sav' % OUTPUT_DIRECTORY
+
+# Can Settings
 CAN_BUS_TYPE = 'socketcan_native'
 CAN_VEHICLE_CHANNEL = 'can1'
 CAN_MABX_CHANNEL = 'can0'
 
-LOG_FILE = 'debug.log'
+# Logging Settings
 LOG_FORMAT = '%(asctime)s => %(message)s'
 
-LAST_GPS_POINT_FILE = 'gps.sav'
-
+# Uploader Settings
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 LOCAL_DB_NAME = 'users_db.sqlite3'
-
-SEVER_IP = '192.168.1.191'
-SEVER_PORT = 8000
-SEVER_ADDRESS = 'http://%s:%d/' % (SEVER_IP, SEVER_PORT)
+SEVER_ADDRESS = 'http://192.168.1.191:8000/'
 SERVER_MAX_PING = 500  # 500 ms
-SESSION_SAVE_FILE = 'sessions.sav'
-SESSION_UPLOAD_FILE = 'to_upload_%d.sav'
 SESSION_UPLOAD_FILE_FILTER = r'^upload_([0-9]+)\.txt$'
 TIME_BETWEEN_UPLOAD = 300  # 5 min
+
+
+# Nfc settings
+GPIO_AUTHORISATION_OUTPUT = 12  # Free GPIO : 12 16 18 19 20 21 23
 
 
 class MODE(Enum):
@@ -34,8 +40,6 @@ class STATUS_CODE(Enum):
     LOGIN = 1
     LOGOUT = 2
 
-
-GPIO_AUTHORISATION_OUTPUT = 12  # Free GPIO : 12 16 18 19 20 21 23
 
 # Load local settings
 try:
