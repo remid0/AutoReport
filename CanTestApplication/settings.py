30e508319f0a7fc1d119da1182c1087108dd03cb
Camille Quenin <camille.quenin@etu.utc.fr>
2018-01-05 10:30:43 +0100

    fix issues in CanAppTest settings (add output)

--- CanTestApplication/settings.py
+++ CanTestApplication/settings.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 # General parameters
 INITIAL_TEST_TYPE = "local"
-IS_MABX_READY = False
+IS_MABX_READY = True
 IS_VEHICLE_READY = True
 MABX_LOGS_FILE = 'logs/can_mabx.txt'
 MAX_ODOMETER_VALUE = 1048574
@@ -12,11 +12,12 @@ VEHICLE_LOGS_FILE = 'logs/can_vehicle.txt'
 #TODO: change the following
 RPI_APPLICATION_PATH = ''
 RPI_BUS_TYPE = 'kvaser'
-RPI_VEHICLE_CHANNEL = 0
-RPI_MABX_CHANNEL = 1
+RPI_VEHICLE_CHANNEL = 1
+RPI_MABX_CHANNEL = 0
 RPI_FILTERS = None
 #TODO: change the two following paths
-RPI_REMOTE_SESSIONS_FILE = '/home/pi/workspace/AutoReport/RaspiClient/sessions.sav'
+#RPI_REMOTE_SESSIONS_FILE = '/home/pi/workspace/AutoReport/RaspiClient/output/sessions.sav'
+RPI_REMOTE_SESSIONS_FILE = '/home/pi/workspace/delete_me/output/sessions.sav'
 RPI_LOCAL_SESSIONS_FILE = '/home/robotex/AutoReport/sessions.sav'
 RPI_ID = 'pi@192.168.1.113'
 
@@ -25,7 +26,7 @@ LOCAL_APPLICATION_PATH= '../RaspiClient/main.py'
 LOCAL_BUS_TYPE = 'socketcan_native'
 LOCAL_VEHICLE_CHANNEL = 'vcan0'
 LOCAL_MABX_CHANNEL = 'vcan1'
-LOCAL_SESSIONS_FILE = '../RaspiClient/sessions.sav'
+LOCAL_SESSIONS_FILE = '../RaspiClient/output/sessions.sav'
 
 class MODE(Enum):
     MANUAL_DRIVING = 'MAN'
